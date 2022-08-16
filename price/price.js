08c54630580eee5b183f3442e876f7dca37ac2a3
Mika Rinne <mika.rinne@oracle.com>
2022-08-16 13:03:16 +0300

    home and price component

--- price/price.js
+++ price/price.js
@@ -2,55 +2,76 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+var free = {};
+free.price = {};
+free.options = {};
+free.price.monthly = 0;
+free.price.users = 10;
+free.price.storage = 2;
+free.price.support = 'Email';
+free.options.public = 1;
+free.options.private = 0;
+free.options.permissions = 1;
+free.options.sharing = 0;
+free.options.unlimited = 0;
+free.options.extrasec = 0;
+
+var pro = {};
+pro.price = {};
+pro.options = {};
+pro.price.monthly = 15;
+pro.price.users = 10;
+pro.price.storage = 10;
+pro.price.support = 'Priority email';
+pro.options.public = 1;
+pro.options.private = 1;
+pro.options.permissions = 1;
+pro.options.sharing = 1;
+pro.options.unlimited = 1;
+pro.options.extrasec = 0;
+
+var enterprise = {};
+enterprise.options = {};
+enterprise.price = {};
+enterprise.price.monthly = 29;
+enterprise.price.users = 30;
+enterprise.price.storage = 15;
+enterprise.price.support = 'Phone and email';
+enterprise.options.public = 1;
+enterprise.options.private = 1;
+enterprise.options.permissions = 1;
+enterprise.options.sharing = 1;
+enterprise.options.unlimited = 1;
+enterprise.options.extrasec = 1;
+
 app.get('/free', (req, res) => {
-  var resp = {};
-  resp.price = {};
-  resp.options = {};
-  resp.price.monthly = 0;
-  resp.price.users = 10;
-  resp.price.storage = 2;
-  resp.price.support = 'Email';
-  resp.options.public = 1;
-  resp.options.private = 0;
-  resp.options.permissions = 1;
-  resp.options.sharing = 0;
-  resp.options.unlimited = 0;
-  resp.options.extrasec = 0;
-  res.send(JSON.stringify(resp));
+  res.send(JSON.stringify(free));
+});
+
+app.put('/free/:json', (req, res) => {
+  var data = JSON.parse(req.params['json']);
+  free = data;
+  console.log(free);
 });
 
 app.get('/pro', (req, res) => {
-  var resp = {};
-  resp.price = {};
-  resp.options = {};
-  resp.price.monthly = 15;
-  resp.price.users = 10;
-  resp.price.storage = 10;
-  resp.price.support = 'Priority email';
-  resp.options.public = 1;
-  resp.options.private = 1;
-  resp.options.permissions = 1;
-  resp.options.sharing = 1;
-  resp.options.unlimited = 1;
-  resp.options.extrasec = 0;
-  res.send(JSON.stringify(resp));
+  res.send(JSON.stringify(pro));
+});
+
+app.put('/pro/:json', (req, res) => {
+  var data = JSON.parse(req.params['json']);
+  pro = data;
+  console.log(pro);
 });
 
 app.get('/enterprise', (req, res) => {
-  var resp = {};
-  resp.options = {};
-  resp.price = {};
-  resp.price.monthly = 29;
-  resp.price.users = 30;
-  resp.price.storage = 15;
-  resp.price.support = 'Phone and email';
-  resp.options.public = 1;
-  resp.options.private = 1;
-  resp.options.permissions = 1;
-  resp.options.sharing = 1;
-  resp.options.unlimited = 1;
-  resp.options.extrasec = 1;
-  res.send(JSON.stringify(resp));
+  res.send(JSON.stringify(enterprise));
+});
+
+app.put('/enterprise/:json', (req, res) => {
+  var data = JSON.parse(req.params['json']);
+  enterprise = data;
+  console.log(enterprise);
 });
 
 app.use('/', express.static(path.join(__dirname, 'html/admin')));
