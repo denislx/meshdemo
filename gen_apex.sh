08abbe9cfff3557f6e8be6c9cabc96a64744221b
Oracle Public Cloud User <opc@dev2.sub02231048050.osokmysql.oraclevcn.com>
2022-09-15 15:39:55 +0000

    APEX changes

--- gen_apex.sh
+++ gen_apex.sh
@@ -2,8 +2,8 @@
 
 export conn=oracledb_tp
 export pwd=atp_pwd
-export schema=DEV
-export wsname=DEV
+export schema=PRICEADMIN
+export wsname=PRICEADMIN
 export application_id=100
 export tables_to_copy=Y
 
@@ -14,8 +14,7 @@ else
     export tablesconfig=""
 fi    
 
-printf "set cloudconfig ./Wallet/Wallet.zip\nconn ${schema}/${pwd}@${conn}\n/\ntables\nlb genschema 
--split\n${tablesconfig}\n" > gen.sql
+printf "set cloudconfig ./Wallet/Wallet.zip\nconn ${schema}/${pwd}@${conn}\n/\ntables\nlb genschema -split\n${tablesconfig}\n" > gen.sql
 
 if [ -n "${application_id}" ]; then
     printf "lb genobject -type apex -applicationid ${application_id} -skipExportDate -expOriginalIds\nexit" >> gen.sql
