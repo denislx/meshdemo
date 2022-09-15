231dcef452f5ac4cd9a25c87b0a14b2fb583eef1
Mika Rinne <mika.rinne@oracle.com>
2022-09-15 11:31:49 +0300

    db creation fix

--- create_apex.sh
+++ create_apex.sh
@@ -9,6 +9,7 @@ export tables_to_copy=Y
 
 
 printf "set cloudconfig ./Wallet/Wallet.zip\nconn admin/${pwd}@${conn}\n/\n" > upd.sql
+printf "delete user ${schema} \n/\n" >> upd.sql
 printf "create user ${schema} identified by \"${pwd}\"\n/\n" >> upd.sql
 printf "GRANT CONNECT, CREATE SESSION, CREATE CLUSTER, CREATE DIMENSION, CREATE INDEXTYPE, CREATE JOB, CREATE MATERIALIZED VIEW, CREATE OPERATOR, CREATE PROCEDURE, CREATE SEQUENCE, CREATE SYNONYM, CREATE TABLE, CREATE TRIGGER, CREATE TYPE, CREATE VIEW to ${schema};\n" >> upd.sql
 printf "ALTER USER ${schema} quota unlimited on DATA;\n/\n" >> upd.sql
