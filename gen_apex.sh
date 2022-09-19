5bb94eaf41aea8021bd0d067330a8e89772070b8
Mika Rinne <mika.rinne@oracle.com>
2022-09-19 10:59:01 +0300

    DNS and APEX fixes

--- gen_apex.sh
+++ gen_apex.sh
@@ -1,12 +1,25 @@
 #!/bin/bash
 
 export conn=oracledb_tp
-export pwd=atp_pwd
+export pwd=
 export schema=PRICEADMIN
 export wsname=PRICEADMIN
 export application_id=100
 export tables_to_copy=Y
+export secret_id=
 
+if [ "${secret_id}" != ""]; then
+    rm -rf ./Wallet
+    mkdir ./Wallet
+    cd ./Wallet
+    export atp_pwd=$(oci secrets secret-bundle get --secret-id ${secret_id} --query "data.\"secret-bundle-content\".content" | tr -d '"' | base64 -d)
+    oci db autonomous-database generate-wallet --autonomous-database-id ${atp_id} --password=${atp_pwd} --file=Wallet.zip
+    unzip Wallet.zip
+    sed -i "s|"?/network/admin"|"./Wallet"|g" sqlnet.ora
+    rm -f Wallet.zip
+    zip Wallet.zip *
+    cd ..
+fi
 
 if [ "${tables_to_copy}" == "Y" ]; then
     export tablesconfig="lb data"
@@ -19,3 +32,5 @@ printf "set cloudconfig ./Wallet/Wallet.zip\nconn ${schema}/${pwd}@${conn}\n/\nt
 if [ -n "${application_id}" ]; then
     printf "lb genobject -type apex -applicationid ${application_id} -skipExportDate -expOriginalIds\nexit" >> gen.sql
 fi
+
+./sqlcl/bin/sql /nolog @./upd_apex.sql
